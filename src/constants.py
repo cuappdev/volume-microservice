55bf5d62805a026e07dedef3a30f9a86ce3637b5
Orko Sinha <37596785+orkosinha@users.noreply.github.com>
2021-11-09 20:07:43 -0500

    Add some init files for magazines

--- src/constants.py
+++ src/constants.py
@@ -1,2 +1,4 @@
 IMAGE_ADDRESS = "https://raw.githubusercontent.com/cuappdev/assets/master/volume/"
+ISU_SOCIAL_ENDPOINT = "https://issuu.com/call/profile/v1/social?username="
+ISU_DOCUMENTS_ENDPOINT = "https://issuu.com/call/profile/v1/documents/"
 STATES_LOCATION = "./.states/"
---
+++ src/content/magazine.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+from datetime import datetime
+from dateutil import parser as date_parser
+
+
+class Article:
+    def __init__(self, entry, slug):
+        self.entry = entry
+        self.slug = slug
+
+    def get_img(self):
+        if "content" in self.entry:
+            soup = BeautifulSoup(self.entry.content[0].value, features="html.parser")
+            img = soup.find("img")
+            return img["src"] if img else ""
+        return ""
+
+    def get_date(self):
+        return date_parser.parse(self.entry.published)
+
+    def serialize(self):
+        return {
+            "articleURL": self.entry.link,
+            "date": self.get_date(),
+            "imageURL": self.get_img(),
+            "publicationSlug": self.slug,
+            "title": self.entry.title,
+        }
