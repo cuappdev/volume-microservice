3df8a13b31087244eeef60c521c5f99f5de9c2b1
Kidus Zegeye <kmz25@cornell.edu>
2022-01-04 03:42:59 -0600

    Merge local branch 'main' of microservice into main

--- src/article.py
+++ src/article.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from constants import PLACEHOLDER_IMAGE_ADDRESS
 from datetime import datetime
 from dateutil import parser as date_parser
 from constants import IMAGE_ADDRESS
@@ -10,19 +11,14 @@ class Article:
         self.publication = publication.serialize()
 
     def get_img(self):
-        for content in getattr(self.entry, "content", []):
-            if content.type == "text/html":
-                soup = BeautifulSoup(content.value, features="html.parser")
-                img = soup.find("img")
-                return (
-                    img["src"]
-                    if img
-                    else IMAGE_ADDRESS
-                    + "placeholders/"
-                    + self.publication.slug
-                    + ".png"
-                )
-        return IMAGE_ADDRESS + "placeholders/" + self.publication.slug + ".png"
+        default_image_url = (
+            PLACEHOLDER_IMAGE_ADDRESS + "/" + self.publication["slug"] + ".png"
+        )
+        if "content" in self.entry:
+            soup = BeautifulSoup(self.entry.content[0].value, features="html.parser")
+            img = soup.find("img")
+            return img["src"] if img else default_image_url
+        return default_image_url
 
     def get_date(self):
         return date_parser.parse(self.entry.published)
