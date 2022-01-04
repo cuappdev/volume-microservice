ca382b9379c2dae5049b56538013b246a819dd17
Tedi Mitiku <tedi.m52@gmail.com>
2022-01-04 04:26:04 -0500

    Implement placeholder images (#10)
    * Implement placeholder images
    
    * revert STATES_LOCATION

--- src/constants.py
+++ src/constants.py
@@ -1,2 +1,3 @@
 IMAGE_ADDRESS = "https://raw.githubusercontent.com/cuappdev/assets/master/volume"
 STATES_LOCATION = "./.states/"
+PLACEHOLDER_IMAGE_ADDRESS = "https://raw.githubusercontent.com/cuappdev/assets/master/volume/placeholders/"
