9769e808005c37a556262c18dbc3432e4d28273d
Kidus Zegeye <51487468+kidzegeye@users.noreply.github.com>
2022-02-17 12:47:28 -0600

    Merge main to release (#13)
    * Implement placeholder images
    
    * Updated get_img
    
    * Update prod deployment workflow
    
    * Disable notifications request
    
    * Implement placeholder images (#10)
    
    * Implement placeholder images
    
    * revert STATES_LOCATION
    
    * Merge local branch 'main' of microservice into main
    
    * Remove deploy_dev.action on pull request (#11)
    
    Co-authored-by: Kidus Zegeye <kmz25@cornell.edu>
    Co-authored-by: Orko Sinha <37596785+orkosinha@users.noreply.github.com>
    Co-authored-by: Tedi Mitiku <tedi.m52@gmail.com>

--- src/article.py
+++ src/article.py
@@ -1,6 +1,8 @@
 from bs4 import BeautifulSoup
+from constants import PLACEHOLDER_IMAGE_ADDRESS
 from datetime import datetime
 from dateutil import parser as date_parser
+from constants import IMAGE_ADDRESS
 
 
 class Article:
@@ -9,11 +11,14 @@ class Article:
         self.publication = publication.serialize()
 
     def get_img(self):
+        default_image_url = (
+            PLACEHOLDER_IMAGE_ADDRESS + "/" + self.publication["slug"] + ".png"
+        )
         if "content" in self.entry:
             soup = BeautifulSoup(self.entry.content[0].value, features="html.parser")
             img = soup.find("img")
-            return img["src"] if img else ""
-        return ""
+            return img["src"] if img else default_image_url
+        return default_image_url
 
     def get_date(self):
         return date_parser.parse(self.entry.published)
