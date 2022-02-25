9a7f137684b09ee78dd3dce7ad9aee61ff26f804
Kidus Zegeye <51487468+kidzegeye@users.noreply.github.com>
2022-02-24 22:39:15 -0600

    Add filter to microservice (#14)
    Co-authored-by: Kidus Zegeye <kmz25@cornell.edu>

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,6 @@
 IMAGE_ADDRESS = "https://raw.githubusercontent.com/cuappdev/assets/master/volume"
 STATES_LOCATION = "./.states/"
-PLACEHOLDER_IMAGE_ADDRESS = "https://raw.githubusercontent.com/cuappdev/assets/master/volume/placeholders/"
+PLACEHOLDER_IMAGE_ADDRESS = (
+    "https://raw.githubusercontent.com/cuappdev/assets/master/volume/placeholders/"
+)
+FILTERED_WORDS = ["covid-19", "coronavirus"]
