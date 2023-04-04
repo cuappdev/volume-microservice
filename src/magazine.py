19195be39761d3833039f3da8d478e476f0a4dab
SashaLoayza <al959@cornell.edu>
2023-04-03 21:37:40 -0400

    Add cover image png to serialization of magazine.

--- src/magazine.py
+++ src/magazine.py
@@ -11,6 +11,7 @@ import io
 import json
 import os
 import requests
+import pypdfium2 as pdfium
 
 
 class Magazine:
@@ -26,9 +27,15 @@ class Magazine:
         self.publication = publication
 
     def download_magazine(self, id):
+        """
+        pdf response and first image response from appdev upload service
+        
+        returns response tuple (<pdf_response>, <first_page_response>):
+        ({"success": <bool>, "data": "<pdf url?>"}, {"success": <bool>, "data": "<first page png url?>"})
+        """
         creds = service_account.Credentials.from_service_account_file(
-            os.getenv("GOOGLE_SERVICE_ACCOUNT_PATH")
-        )
+            os.getenv("GOOGLE_SERVICE_ACCOUNT_PATH"))
+        
         try:
             # create drive api client
             service = build("drive", "v3", credentials=creds)
@@ -40,31 +47,52 @@ class Magazine:
             done = False
             while done is False:
                 status, done = downloader.next_chunk()
-
+                
         except HttpError as error:
             print(f"An error occurred: {error}")
             file = None
+            
         if file is not None:
-            payload = json.dumps(
-                {
-                    "bucket": UPLOAD_BUCKET,
-                    "image": "data:application/pdf;base64,"
-                    + base64.b64encode(file.getvalue()).decode("utf-8"),
-                }
-            )
-            return requests.post(
-                "https://upload.cornellappdev.com/upload/",
-                payload,
-            ).text
+            pdf = pdfium.PdfDocument(file)
+            first_page = pdf[0]
+            pil_image = first_page.render().to_pil().resize((150,220))
+
+            imageBytes = io.BytesIO() #create bytes stream to hold image
+            pil_image.save(imageBytes, format='png')
+            
+            image_payload = json.dumps(
+            {
+            "bucket": "volume",
+            "image": "data:image/png;base64,"
+            + base64.b64encode(imageBytes.getvalue()).decode("utf-8")})
+            
+            image_response = requests.post("https://upload.cornellappdev.com/upload/",image_payload).text
+
+            pdf_payload = json.dumps(
+                {"bucket": UPLOAD_BUCKET,
+                "image": "data:application/pdf;base64,"
+                + base64.b64encode(file.getvalue()).decode("utf-8")})
+            
+            pdf_response  = requests.post("https://upload.cornellappdev.com/upload/",pdf_payload).text
+            
+            return (pdf_response, image_response)
         return None
 
     def get_date(self, date):
         return date_parser.parse(date)
 
     def serialize(self):
-        response = self.download_magazine(id)
-        response = json.loads(response)
-        if response["success"]:
+        pdf_response = self.download_magazine(id)[0]
+        image_response = self.download_magazine(id)[1]
+        
+        image_response_obj = json.loads(image_response)
+        if image_response_obj["success"]:
+            image_url = image_response_obj["data"]
+        else:
+            raise Exception
+        pdf_response_obj = json.loads(pdf_response)
+        if pdf_response_obj["success"]:
+            pdf_url = pdf_response_obj["data"]
             return {
                 "date": self.get_date(self.timestamp),
                 "published": self.date_pub,
@@ -72,7 +100,8 @@ class Magazine:
                 "title": self.title,
                 "publicationSlug": self.slug,
                 "publication": self.publication,
-                "pdfURL": response["data"],
+                "pdfURL": pdf_url,
+                "imageURL": image_url,
                 "isFiltered": self.is_profane(),
             }
         else:
