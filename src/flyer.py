ff8b11089d68192908833db0f4dcdaf03ae9ca1a
Isaac Han <isaac.han100@gmail.com>
2023-06-26 08:29:12 -0400

    Isaac/merge main release (#31)
    * Remove RSS feed for Daily Sun, fix for Healthcare Review (#21)
    
    * Add 3 new publications (Cornell Daily Sun, Collective X, and Cornell Healthcare Review) to publications.json
    
    * Remove problematic Daily Sun rss feed and fix Healthcare Review feed.
    
    * Add organizations to database (#23)
    
    - implemented logic to add organizations to database
    - added three test organizations to organizations.json
    
    * Parse Cornell Daily Sun pubDate and add Daily Sun rss feed back. (#24)
    
    * Implement community board [2/3] (#25)
    
    * Add organizations to database
    
    - implemented logic to add organizations to database
    - added three test organizations to organizations.json
    
    * Implement flyer collection on microservice
    
    - implemented flyer collection on microservice in similar way to magazines
    - factored out downloading content function from magazine and added it to utils
    - updated app.py to collect magazines
    - created flyer class
    
    * Update utils.py
    
    - enabled downloading images from pdfs in future pr
    - updated utils.py to allow downloading an image from a pdf
    - updated requirements.txt with libraries needed to download images from pdfs
    
    * small syntax fix
    
    * Address pr review comments
    
    - added link for prod flyer sheet
    
    * Add cover image png to serialization of magazine. (#22)
    
    * Add cover image png to serialization of magazine.
    
    * Fix style in magazine.py
    
    * Keep cornell sun RSS feed url
    
    As Isaac commented, the RSS feed url is included in a later commit as the issue is resolved.
    
    * Refactor magazine.py to use utils.py
    
    * Implement community board (onboard organizations) [3/3] (#26)
    
    * Add organizations to database
    
    - implemented logic to add organizations to database
    - added three test organizations to organizations.json
    
    * Implement flyer collection on microservice
    
    - implemented flyer collection on microservice in similar way to magazines
    - factored out downloading content function from magazine and added it to utils
    - updated app.py to collect magazines
    - created flyer class
    
    * Update utils.py
    
    - enabled downloading images from pdfs in future pr
    - updated utils.py to allow downloading an image from a pdf
    - updated requirements.txt with libraries needed to download images from pdfs
    
    * small syntax fix
    
    * Initial commit
    
    * onboard more publications
    
    * Onboard organizations
    
    - added all current onboarded organizations to organizations.json
    
    * add start and end dates to flyer collection (#29)
    
    * update flyer organizations and organizationSlugs fields (#30)
    
    * add start and end dates to flyer collection
    
    * Make organization field of flyer a list
    
    * parse dates as isoformat for flyers
    
    ---------
    
    Co-authored-by: Sasha Loayza <104698418+SashaLoayza@users.noreply.github.com>

--- src/flyer.py
+++ src/flyer.py
@@ -1,8 +1,10 @@
 from better_profanity import profanity as pf
 from constants import FILTERED_WORDS
 
+import dateutil.parser as parser
 import json
 import utils
+
 
 class Flyer:
     def __init__(self, sheet_row, organization):
@@ -15,27 +17,28 @@ class Flyer:
         self.location = sheet_row[8]
         self.image_link = sheet_row[9]
         self.flyer_link = sheet_row[10]
-        self.file_id = self.image_link[self.image_link.index("id=") + 3 :]
+        self.file_id = self.image_link[self.image_link.index("id=") + 3:]
         self.organization = organization
 
     def upload_image(self, imageBase64):
         return imageBase64
-    
+
     def is_profane(self):
         return pf.contains_profanity(self.title)
-    
+
     def serialize(self):
         response_bytes = utils.download_bytes(self)
         response = json.loads(utils.download_pdf(response_bytes))
         if response["success"]:
-          return {
-              "date": self.date,
-              "flyerURL": self.flyer_link,
-              "imageURL": response["data"],
-              "location": self.location,
-              "organization": self.organization,
-              "organizationSlug": self.org_slug,
-              "title": self.title
-          }
+            return {
+                "startDate": parser.parse(self.date + "/" + self.start_time).isoformat(),
+                "endDate": parser.parse(self.date + "/" + self.end_time).isoformat(),
+                "flyerURL": self.flyer_link,
+                "imageURL": response["data"],
+                "location": self.location,
+                "organizations": [self.organization],
+                "organizationSlugs": [self.org_slug],
+                "title": self.title
+            }
         else:
             raise Exception
